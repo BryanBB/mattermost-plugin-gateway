034485d94626b88f01a138bc06a18d4b77169704
Bryn <bryn@BryndeMac-mini.local>
2023-07-18 19:49:37 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mattermost/mattermost-plugin-profanity-filter
+
+go 1.14
+
+require (
+	github.com/mattermost/mattermost-server/v5 v5.23.1
+	github.com/mholt/archiver/v3 v3.3.0
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.2
+)
---
+++ server/plugin.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"sync"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+)
+
+type Plugin struct {
+	plugin.MattermostPlugin
+
+	// configurationLock synchronizes access to the configuration.
+	configurationLock sync.RWMutex
+
+	// configuration is the active plugin configuration. Consult getConfiguration and
+	// setConfiguration for usage.
+	configuration *configuration
+
+	badWordsRegex *regexp.Regexp
+}
+
+func (p *Plugin) FilterPost(post *model.Post) (*model.Post, string) {
+	configuration := p.getConfiguration()
+	_, fromBot := post.GetProps()["from_bot"]
+
+	if configuration.ExcludeBots && fromBot {
+		return post, ""
+	}
+
+	postMessageWithoutAccents := removeAccents(post.Message)
+
+	if !p.badWordsRegex.MatchString(postMessageWithoutAccents) {
+		return post, ""
+	}
+
+	detectedBadWords := p.badWordsRegex.FindAllString(postMessageWithoutAccents, -1)
+
+	if configuration.RejectPosts {
+		p.API.SendEphemeralPost(post.UserId, &model.Post{
+			ChannelId: post.ChannelId,
+			Message:   fmt.Sprintf(configuration.WarningMessage, strings.Join(detectedBadWords, ", ")),
+			RootId:    post.RootId,
+		})
+
+		return nil, fmt.Sprintf("Profane word not allowed: %s", strings.Join(detectedBadWords, ", "))
+	}
+
+	for _, word := range detectedBadWords {
+		post.Message = strings.ReplaceAll(
+			post.Message,
+			word,
+			strings.Repeat(p.getConfiguration().CensorCharacter, len(word)),
+		)
+	}
+
+	return post, ""
+}
+
+func (p *Plugin) MessageWillBePosted(_ *plugin.Context, post *model.Post) (*model.Post, string) {
+	return p.FilterPost(post)
+}
+
+func (p *Plugin) MessageWillBeUpdated(_ *plugin.Context, newPost *model.Post, _ *model.Post) (*model.Post, string) {
+	return p.FilterPost(newPost)
+}
+
+func removeAccents(s string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	output, _, e := transform.String(t, s)
+	if e != nil {
+		return s
+	}
+
+	return output
+}
